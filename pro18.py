f646b84043643aa28a42c4535ab2c4a9cc6576dd
monishramji <50619799+monishramji@users.noreply.github.com>
2019-07-08 20:45:55 +0530

    Create pro18.py

---
+++ pro18.py
@@ -0,0 +1,16 @@
+n1,m1=map(int,input().split())
+tem1=[]
+x=0
+for m in range(n1):
+    tem1.append(list(map(int,input().split())))   
+for m in range(n1):
+    for j in range(m1-1):
+        for k in range(j+1,m1+1):
+            if tem1[m][j:k]==[1]*len(tem1[m][j:k]):
+                 if all(tem1[p+m][j:k]==[1]*len(tem1[p+m][j:k]) for p in range(len(tem1[m][j:k])-1)):
+                     if len(tem1[m][j:k])>x:
+                        x=len(tem1[m][j:k])
+if len(tem1)==1 and len(tem1[0])==1 and tem1[0][0]==1:
+    print(1)
+for m in range(x):
+    print(*[1]*x) 
